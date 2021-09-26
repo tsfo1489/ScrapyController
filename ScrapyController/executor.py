602947496d45d7b2ff6bb74b2bd574e6613eae32
Arepo <tsfo1489@gmail.com>
2021-09-27 00:16:31 +0900

    Refactor: Refactoring executor function
     - Remove unnecessary print
     - Change double quotation to single quotation
     - Rename function
     - Delete Main function

--- ScrapyController/executor.py
+++ ScrapyController/executor.py
@@ -1,40 +1,23 @@
 import json
 import subprocess
-from logging import handlers
 
-def readJson(name):
+def json_to_cmd(name):
     result = []
-    base = "scrapy crawl"
+    base = ['scrapy', 'crawl']
     with open(name,'r') as file:
         json_data = json.load(file)
-    data = json.dumps(json_data)
-    #print("data", data)
+
     for crawler in json_data:
-        base =base + " " + crawler["name"]
-        addArgs= ""
-        addOptions = ""
-        parameters = crawler["parameters"]
-        for arg in parameters:
-            addArgs = addArgs + " -a "+ arg +"="+parameters[arg]
-        options = crawler["options"]
-        for opt in options :
-            addOptions = addOptions + " -s "+opt+"="+options[opt]
-        """
-        print("base: ", base)
-        print("Args: ", addArgs)
-        print("Opts: ", addOptions)
-        print(base + addArgs + addOptions)
-        """
-        result.append(base + addArgs + addOptions)
+        parameters = crawler['parameters']
+        parameters = [f'-a {param}={parameters[param]}' for param in parameters]
+        options = crawler['options']
+        options = [f'-s {opt}={options[opt]}' for opt in options]
+
+        cmds = base + [crawler['name']] + parameters + options
+        result.append(' '.join(cmds))
     return result
 
-def executer():
-    jsonFileName = "practice.json"
-    para = readJson(jsonFileName)
-    #print("para: ", para)
+def executor(json_src : str):
+    para = json_to_cmd(json_src)
     for cline in para:
-        process = subprocess.Popen(cline.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
-
-if __name__ == '__main__':
-    executer()   
+        proc = subprocess.Popen(cline.split(), stdout=subprocess.PIPE)

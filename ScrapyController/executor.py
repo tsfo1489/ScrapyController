413dcb909aa83f2ccd3c400fe4e3bbc36e939b04
Arepo <tsfo1489@gmail.com>
2021-10-28 22:14:45 +0900

    Fix: Executor 변경
     - 임시 로그 생성, 로그 파일명은 설정값과 현재 시간을 해싱하여 랜덤 생성
     - json에서 하나의 크롤러 정보만 입력 받도록 수정
     - executor가 pid와 로그 설정을 return함

--- ScrapyController/executor.py
+++ ScrapyController/executor.py
@@ -1,23 +1,35 @@
 import json
 import subprocess
+import hashlib
+from datetime import datetime
 
-def json_to_cmd(name):
-    result = []
+def json_to_cmd(json_data):
     base = ['scrapy', 'crawl']
-    with open(name,'r') as file:
-        json_data = json.load(file)
+    parameters = json_data['parameters']
+    parameters = [f'-a {param}={parameters[param]}' for param in parameters]
+    options = json_data['options']
+    final_log = {}
+    if 'LOG_FILE' in options:
+        final_log['LOG_FILE'] = options['LOG_FILE']
+        options.pop('LOG_FILE') # Delete Log File option
+    if 'LOG_LEVEL' in options:
+        final_log['LOG_LEVEL'] = options['LOG_LEVEL']
+        options.pop('LOG_LEVEL') # Delete Log Level option
+    options = [f'-s {opt}={options[opt]}' for opt in options]
 
-    for crawler in json_data:
-        parameters = crawler['parameters']
-        parameters = [f'-a {param}={parameters[param]}' for param in parameters]
-        options = crawler['options']
-        options = [f'-s {opt}={options[opt]}' for opt in options]
+    cmds = base + [json_data['name']] + parameters + options
+    cmd = ' '.join(cmds)
+    temp_hash = hashlib.md5(cmd.encode())
+    temp_hash.update(str(datetime.now().timestamp()).encode())
+    temp_log = temp_hash.hexdigest()
+    cmd += f' -s LOG_FILE={temp_log}.log'
+    final_log['TEMP_LOG'] = temp_log + '.log'
+    return cmd, final_log
 
-        cmds = base + [crawler['name']] + parameters + options
-        result.append(' '.join(cmds))
-    return result
+def executor(json_data: dict):
+    cmd, log_setting = json_to_cmd(json_data)
+    proc = subprocess.Popen(cmd.split())
+    return proc, log_setting
 
-def executor(json_src : str):
-    para = json_to_cmd(json_src)
-    for cline in para:
-        proc = subprocess.Popen(cline.split(), stdout=subprocess.PIPE)
+if __name__ == '__main__':
+    executor(json.loads(open('test.json', 'r').read()))
